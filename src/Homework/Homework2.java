c665915118bca43a4cdc45de9fa32ab9fef28625
Keezill <mamedov.murik.lezgin@gmail.com>
2021-05-02 20:42:25 +0300

    Fixed square formula

--- src/Homework/Homework2.java
+++ src/Homework/Homework2.java
@@ -2,18 +2,15 @@ package Homework;
 
 public class Homework2 {
     public static void main(String[] args) {
-        getSquareOfTriangle();
+        getSquareOfTriangle(1,1,2,5,5,3);
         checkIfNumberEven(51);
         getSmallestModulusOfNumber(-1.2, -2.6, 8.1);
     }
 
-    private static void getSquareOfTriangle() {
-        int x = 3;
-        int y = 5;
-        int z = 4;
+    private static void getSquareOfTriangle(int x1, int y1, int x2, int y2 , int x3, int y3) {
 
-        int result = x * y * z;
-        System.out.println("Square of triangle: " + result);
+        int square = Math.abs((x1 - x3) * (y2 - y1) - (x1 - x2) * (y3 - y1)) / 2;
+        System.out.println("Square of triangle: " + square);
 
         System.out.println();
     }
