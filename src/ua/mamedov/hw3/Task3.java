76139fed594c87b5cd6742f93b24930f5dab023b
Keezill <mamedov.murik.lezgin@gmail.com>
2021-05-07 18:58:09 +0300

    Corrected word search

--- src/ua/mamedov/hw3/Task3.java
+++ src/ua/mamedov/hw3/Task3.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         String text = new Scanner(System.in).nextLine();
-        System.out.println(countWords(text));
+        System.out.println("Text contains " + countWords(text) + " words");
     }
 
     private static int countWords(String text) {
         int count = 1;
         if (text.length() != 0) {
             for (int i = 0; i < text.length(); i++) {
-                if (text.charAt(i) == ' ') {
+                if (text.charAt(i) == ' ' && text.charAt(i - 1) != ' ') {
                     count++;
                 }
             }
