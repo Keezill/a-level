b7d0f2b767a03465771ee0d7b9ff28db2022e960
Keezill <mamedov.murik.lezgin@gmail.com>
2021-05-11 19:15:05 +0300

    Displayed array before changes and after them

--- src/ua/mamedov/hw4/Task4.java
+++ src/ua/mamedov/hw4/Task4.java
@@ -1,11 +1,13 @@
 package ua.mamedov.hw4;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task4 {
     public static void main(String[] args) {
         int[] array = new int[2000];
         int[] filledArray = fill(array);
+        System.out.println(Arrays.toString(filledArray));
         System.out.println("All " + changeEvenToZero(filledArray) + " even number is changed to 0");
     }
 
@@ -17,6 +19,7 @@ public class Task4 {
                 count++;
             }
         }
+        System.out.println(Arrays.toString(array));
         return count;
     }
 
